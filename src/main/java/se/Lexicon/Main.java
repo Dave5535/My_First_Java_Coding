a5c867e7ce13b57d417dbbe583678e6d915f3801
David Svantesson <pelleitorp2@gmail.com>
2022-10-10 16:24:03 +0200

    Upplode 1.3 I did add a age test and a salory test and it took some time because I did have one symbol at the wrong place.

--- src/main/java/se/Lexicon/Main.java
+++ src/main/java/se/Lexicon/Main.java
@@ -4,9 +4,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("David!");
+
+        int age= 20;
+    System.out.println ("age " + age );
+
+    double salory = 3000.00;
+    System.out.println ("salory" + salory);
+
+
+
+
+
     }
-
-
 }
-
 
