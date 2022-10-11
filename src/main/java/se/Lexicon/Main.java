69b27fa9cda633546d50e1a0d7716352523e396c
David Svantesson <pelleitorp2@gmail.com>
2022-10-11 10:56:34 +0200

    Upplode 1.5 First uppload today. I did scrap the leapyear code did get stuck on it to much. So I did a simpel calculator as the task NR.3 there you can use simpel comands like ,-,+,*,/ and have 2 numbers as an input.

--- src/main/java/se/Lexicon/Main.java
+++ src/main/java/se/Lexicon/Main.java
@@ -8,37 +8,52 @@ public class Main {
         System.out.println("David!");
 
         // 2 test age and salory
-        int age= 20;
+        int age = 20;
         System.out.println("age " + age);
 
         double salory = 3000.00;
         System.out.println("salory" + salory);
 
         // a test if you change testage from 20 to 10 you get the other message.
-        int testage= 20;
+        int testage = 20;
         if (testage >= 18) {
             System.out.println("You can drive a car");
-        }else {System.out.println("You can't drive the car");
-
-        //Task NR 2 Is it a leap_year?
-            // if year is divisible by 400 then is_leap_year
-            //else if year is divisible by 100 then not_leap_year
-            //else if year is divisible by 4 then is_leap_year
-            //else not_leap_year
-
-            int leapyear= 400;
-            if (leapyear / 400) {System.out.println("yes it was leapyear" + leapyear);
-
-            } else if (leapyear / 100) {System.out.println("It was not a leapyear" + leapyear);
-
-            } else if (leapyear / 4) {System.out.println("Yes it was leapyear" + leapyear);
-
-            } else {System.out.println("It was not a leapyear"+ leapyear);
-
-
+        } else {
+            System.out.println("You can't drive the car");
         }
 
+        //task NR.3 ( small calculatior )
+        // This is a simpel calculator whit +,-,/,*
+        // That you as the operatior can youreself make the inputs
+        double n1 = 24, n2 = 6, result = 0;
+        char operatior = '/';
 
+        switch (operatior) {
+            case '+':
+                result = n1 + n2;
+                break;
+            case '-':
+                result = n1 - n2;
+                break;
+            case '/':
+                result = n1 / n2;
+                break;
+            case '*':
+                result = n1 * n2;
+            default:
+                System.out.println("operatior  is not valid!");
         }
+        System.out.println(result);
+
+
+    }
+
+
+}
+
+
+
+
+
 
 
