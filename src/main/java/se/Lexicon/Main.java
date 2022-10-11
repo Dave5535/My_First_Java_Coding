b33cb013c7e95b06d91cb66feadc696c18a11b15
David Svantesson <pelleitorp2@gmail.com>
2022-10-11 11:21:28 +0200

    Upplode 1.6 I did the task NR.4 of make a program that takes the average number out of 3 random numbers and i did uppdate some organising on the names to all tasks i have done.

--- src/main/java/se/Lexicon/Main.java
+++ src/main/java/se/Lexicon/Main.java
@@ -2,19 +2,21 @@ package se.Lexicon;
 
 public class Main {
 
-    //First task NR 1.
+    //First task NR 1
+    //Print Hello and your name.
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("David!");
 
-        // 2 test age and salory
+        // 2 test age and salory.
         int age = 20;
         System.out.println("age " + age);
 
         double salory = 3000.00;
         System.out.println("salory" + salory);
 
-        // a test if you change testage from 20 to 10 you get the other message.
+        // A test if you awnser depends on what number you put in testage value.
+        // If below 18 you get a different awnser.
         int testage = 20;
         if (testage >= 18) {
             System.out.println("You can drive a car");
@@ -24,7 +26,7 @@ public class Main {
 
         //task NR.3 ( small calculatior )
         // This is a simpel calculator whit +,-,/,*
-        // That you as the operatior can youreself make the inputs
+        // That you as the operatior can youreself make the inputs.
         double n1 = 24, n2 = 6, result = 0;
         char operatior = '/';
 
@@ -44,6 +46,13 @@ public class Main {
                 System.out.println("operatior  is not valid!");
         }
         System.out.println(result);
+
+        //Task NR. 4
+        //average on 3 different numbers.
+
+        double number1 = 2, number2 = 16, number3 = 4, res = 0;
+        res = number1 + number2 + number3;
+        System.out.println(res/3);
 
 
     }
